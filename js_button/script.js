75940dfa006b29574fcb43ba7027c9b12eee3534
mr5354 <42981946+mr5354@users.noreply.github.com>
2022-11-14 09:57:47 -0500

    grow_caterpillar

---
+++ js_button/script.js
@@ -0,0 +1,7 @@
+var caterpillar = new Array();
+
+function grow() {
+    var x = document.createElement("img");
+    x.src = "assets/c_body.png";
+    document.getElementById("c_body").appendChild(x);
+}
